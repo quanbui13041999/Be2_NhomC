a103d118a2cb606d10bf825c6b9bc9a2556af202
kaido <leanhtuan02072006@gmail.com>
2026-04-15 00:12:28 +0700

    ham hien thi fomr dang nhap

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,16 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Psy\ManualUpdater\Checker;
 
 class AuthController extends Controller
 {
-    
+    // Hiển thị form đăng nhập
+    public function showLoginFormr() {
+        if (Auth::check()) {
+            return redirect('/');
+        }
+        return view('login');
+
+    }
 }
