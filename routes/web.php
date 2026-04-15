caab13538eda5b1c468304942d98387bb3eb58f1
ken248 <anh2482006@gmail.com>
2026-04-15 15:42:08 +0700

    làm chức năng đăng kí (đã hoàn thành)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Controllers_register;
+
+Route::get('/register', [Controllers_register::class, 'index']);
+Route::post('/register', [Controllers_register::class, 'store']);
